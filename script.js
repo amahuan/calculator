ffb653fbf37ee742023402f8def3a81439c56b3e
Amanda Huang <aamandahuang@gmail.com>
2022-03-12 23:41:47 -0500

    code now enabled for all features and correct calculations, as well as input by keyboard although plus sign needs to be tweaked

--- script.js
+++ script.js
@@ -14,22 +14,19 @@ const decButton=document.querySelector('.decimal');
 const equalsButton=document.getElementById('enter');
 const clearButton=document.getElementById('clear');
 
+window.addEventListener('keydown',clickToKey);
+
 numButtons.forEach((numButton) => {
     numButton.addEventListener('click', ()=> numInput(numButton.value));
 });
 
 opButtons.forEach((opButton) => {
     opButton.addEventListener('click', () => selectOp(opButton.value));
- });
-
-delButton.addEventListener('click', (e)=> {
-    display.textContent=display.textContent.slice(0,-1);
 });
 
-decButton.addEventListener('click', (e)=> {
-    display.textContent+='.';
-    decButton.disabled=true;
-});
+delButton.addEventListener('click', deleteNum);
+
+decButton.addEventListener('click', addDecimal);
    
 equalsButton.addEventListener('click', evaluate);
 
@@ -47,7 +44,6 @@ function numInput(number){
       decClicked=true;
     }
 }
-
 //if the number that is entered is the first operand (no operation has been assigned yet), it will be assigned to firstOperand and the operation will be 
 //assigned as the callback.  opDisplay will change to reflect the current operation.  Display is reset for next input for second operand.  Decimal button
 // is also re-enabled.
@@ -71,6 +67,10 @@ function evaluate(e){
     secondOperand=display.textContent;    
     if(this.textContent==="ENTER"){
         opDisplay.textContent="=";
+    }
+    if (callback==='÷'&&display.textContent==='0') {
+        display.textContent="invalid operation";
+        return;
     }
     else{
         opDisplay.textContent=callback;
@@ -113,12 +113,21 @@ function add(a, b) {
     return a / b
   }
 
+//deletes last digit each click 
+  function deleteNum(){
+    display.textContent=display.textContent.slice(0,-1);
+}
 //resets the display when needed (After first operand has been inputted)
  function resetDisplay(){
     opDisplay.textContent='';
     display.textContent='';
     shouldResetDisplay=false;
  } 
+//adds decimal point to value on display
+ function addDecimal() {
+    display.textContent+='.';
+    decButton.disabled=true;
+}
 //resets all values wen CLEAR is clicked
  function reset(){
     opDisplay.textContent='';
@@ -133,89 +142,36 @@ function roundResult(number) {
     return Math.round(number * 1000000) / 1000000;
   }
 
-//allows for highlighting of the numbers and operators that are being clicked/keyed
+//allows for highlighting of the numbers and operators that are being keyed
 function highlight(e){
-    var el = document.getElementById(`${e.key}`);
-    var original = el.style.backgroundColor;
+    const el = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    const original = el.style.backgroundColor;
+    if(!el) return;
     el.style.backgroundColor='rgb(169, 238, 255)';
     setTimeout(function() { el.style.backgroundColor = original; }, 100);
 }
 
-// function inputs(e){
-//     if((e.key!=="+")||(e.key!=="-")||(e.key!=="*")||(e.key!=="/")||(e.key!==".")||(e.key!=="Backspace")||(e.key!=="Enter")){
-//         if(num1===null){
-//             num1=e.key;
-//         }
-//         else{
-//             num2=e.key;
-//         }
-//     if(e.key==="+"){
-//         callback=add;
-//         num1=0;
-//     }
-//     else if(e.key==="-"){
-//         callback=subtract;
-//         num1=0;
-//     }
-//     else if(e.key==="*"){
-//         callback=multiply;
-//         num1=0;
-//     }
-//     else if (e.key==="/"){
-//         callback=divide;
-//         num1=0;
-//     }    
-//     if(e.key!=="Enter"){
-//         operate(num1,callback,num2);
-//     }
-// }
-// }
+document.addEventListener('keydown', function (e) {   
+        highlight(e);
+    });
 
+//assigns click functions to keydown events
+function clickToKey(e) {
+console.log(e);
+    if((e.keyCode==="+")||(e.key==="-")||(e.key==="*")||(e.key==="/")){
+        selectOp(convertOperator(e.key));
+    }
+    if(e.key==="Enter"){evaluate();
+    }
+    if(e.key>=0&&e.key<=9){numInput(e.key);
+    }
+    if(e.key==='.'){addDecimal();}
+    if(e.key==="Backspace"){deleteNum();}
+}
 
-// function numbers(e){
-//     if((e.key!=="+")||(e.key!=="-")||(e.key!=="*")||(e.key!=="/")||(e.key!==".")||(e.key!=="Backspace")||(e.key!=="Enter")){
-//         if(num1===undefined){
-//             num1=Number(e.key);
-//         }
-//         else{
-//             num2=Number(e.key);
-//         }    
-//     }
-// }
-
-// function operator(e){
-//         if(e.key==="+"){
-//             callback=add;
-//         }
-//         else if(e.key==="-"){
-//             operate(num1,subtract,num2);
-//         }
-//         else if(e.key==="*"){
-//             callback=multiply;
-//         }
-//         else if (e.key==="/"){
-//             callback=divide;
-//         }      
-//     }
-
-// [num1,function,num2]
-
-// array.reduce(callback(num1,num2))
-
-
-//enter number
-    //if array is empty -> num1 (array[0])
-//enter operation 
-    //saves num1 as array element 0
-    //stores callback based on sign as array element 1
-        //if array isn't empty, stores as array element 2, then assigns to num2
-//either hit enter
-    //performs calculation, updates display, removes elements from array other than result which will be used as num1 in next calculation
-//or hit operation to continue with calculation
-    //push to array (array1)
-    //updates display with current total
-        //enter num which will be stored as num2/array[2]
-    
-    
-    
-     
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator==='/') return '÷'
+    if (keyboardOperator==='*') return '×'
+    if (keyboardOperator==='-') return '−'
+    if (keyboardOperator==='+') return '+'
+  }
